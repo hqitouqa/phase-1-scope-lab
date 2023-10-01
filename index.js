1d78f563890c5da7057cfc34b2446784fa014628
Hala Qitouqa <h.qitouqa@gmail.com>
2023-10-01 19:03:40 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName='Sally'
+const leastFavCustomer='John'
+function upperCaseCustomerName(){
+   return customerName.toUpperCase()   
+    
+}
+console.log(upperCaseCustomerName())
+function setBestCustomer(){
+     bestCustomer='not bob'
+     return bestCustomer
+
+}
+console.log(setBestCustomer())
+
+function overwriteBestCustomer(){
+    bestCustomer='Nanc'
+  return bestCustomer
+}
+console.log(overwriteBestCustomer())
+function changeLeastFavoriteCustomer(){
+     leastFavCustomer='Danny'
+  return leastFavCustomer
+}
+console.log(changeLeastFavoriteCustomer())
+  
